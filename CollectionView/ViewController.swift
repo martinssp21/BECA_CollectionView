77e1c83330c766e670d850014b7586710d1810ef
Rodrigo Martins <rodrigo.martins1@everis.com>
2019-04-24 09:33:42 -0300

    first commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Rodrigo Martins on 08/04/19.
+//  Copyright © 2019 Rodrigo Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
